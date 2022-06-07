b87db6bd0da8ccd6e2ad7d88e4276c04cd4d9141
dnjswnzzang <dnjswnzzang@gmail.com>
2022-06-07 13:09:45 +0900

    초기 커밋.

---
+++ resource.h
@@ -0,0 +1,53 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// NetworkPacketCapture.rc에서 사용되고 있습니다.
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_NETWORKPACKETCAPTURE_DIALOG 102
+#define IDI_ICON1                       126
+#define IDI_ICON2                       127
+#define IDR_MAINFRAME                   128
+#define IDD_CHOICENETWORKINTERFACE_DIALOG 133
+#define IDR_ACCELERATOR1                135
+#define IDR_TOOLBAR1                    140
+#define IDR_MENU1                       143
+#define IDD_DIALOG1                     146
+#define IDC_LIST1                       1000
+#define IDC_PROGRESS1                   1005
+#define IDC_NETWROKINTERFACE            1006
+#define IDC_PACKET_INFO                 1007
+#define IDC_START_BUTTON                1008
+#define IDC_STOP_BUTTON                 1009
+#define IDC_CLEAR_BUTTON                1010
+#define IDC_LIST2                       1018
+#define IDC_FILTER_EDIT                 1020
+#define IDC_FILTER_BUTTON               1024
+#define IDC_SAVE_BUTTON                 1027
+#define IDC_EDIT4                       1028
+#define IDC_EDIT5                       1029
+#define IDC_EDIT6                       1030
+#define IDC_EDIT7                       1031
+#define ID_ACCELERATOR32771             32771
+#define ID_TSTART_BUTTON                32774
+#define ID_TSTOP_BUTTON                 32775
+#define ID_TCLEAR_BUTTON                32776
+#define ID_32777                        32777
+#define ID_32778                        32778
+#define ID_source_button                32779
+#define ID_LOG_BUTTON                   32780
+#define ID_GITHUB_BUTTON                32781
+#define ID_32782                        32782
+#define ID_CHANGE_COLOR                 32783
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        148
+#define _APS_NEXT_COMMAND_VALUE         32784
+#define _APS_NEXT_CONTROL_VALUE         1028
+#define _APS_NEXT_SYMED_VALUE           105
+#endif
+#endif
